6f089e8d4d455a09f8fa80164afa75019acf3625
gadjgasan1997 <gadj.gasan1997@gmail.com>
2024-08-25 22:11:32 +0300

    Добавил модель базового ответа на Rpc запрос

---
+++ src/Essentials.RabbitMqClient/Publisher/Models/BaseRpcResponse.cs
@@ -0,0 +1,38 @@
+﻿namespace Essentials.RabbitMqClient.Publisher.Models;
+
+/// <summary>
+/// Базовый ответ на Rpc запрос
+/// </summary>
+public class BaseRpcResponse
+{
+    /// <summary>
+    /// Конструктор
+    /// </summary>
+    /// <param name="success">Признак успеха</param>
+    /// <param name="responseCode">Код ответа</param>
+    /// <param name="responseMessage">Сообщение с ответом</param>
+    public BaseRpcResponse(
+        bool success,
+        int responseCode,
+        string responseMessage)
+    {
+        Success = success;
+        ResponseCode = responseCode;
+        ResponseMessage = responseMessage;
+    }
+
+    /// <summary>
+    /// Признак успеха
+    /// </summary>
+    public bool Success { get; }
+    
+    /// <summary>
+    /// Код ответа
+    /// </summary>
+    public int ResponseCode { get; }
+    
+    /// <summary>
+    /// Сообщение с ответом
+    /// </summary>
+    public string ResponseMessage { get; }
+}
